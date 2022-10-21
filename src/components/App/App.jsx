c4ac16ab8c10e2da7ac0c7cf3164c2e72135d0f6
ArtemPrihnich <artem.prihnich@gmail.com>
2022-10-21 18:36:54 +0300

    bug fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ContactsForm from '../ContactsForm/ContactsForm';
 import ContactsList from '../ContactsList/ContactsList';
