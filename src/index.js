8a013bebf20214be70e254659083e9e44f78c834
ArtemPrihnich <artem.prihnich@gmail.com>
2022-10-22 15:48:19 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './utils/theme'
 import { Provider } from 'react-redux'
-import store, {persistor} from './redux/store'
+import store from './redux/store'
 // import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
